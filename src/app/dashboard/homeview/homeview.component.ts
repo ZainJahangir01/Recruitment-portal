d0e8e0e7741d4a833051e437269f018a83f23e33
Zain Sheikh <70570850+ZainJahangir01@users.noreply.github.com>
2022-09-12 16:11:07 +0500

    Dashboard-UI
    UI-of-dashboard completed

--- src/app/dashboard/homeview/homeview.component.ts
+++ src/app/dashboard/homeview/homeview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color } from 'ag-grid-community';
 
 @Component({
   selector: 'app-homeview',
@@ -7,17 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeviewComponent implements OnInit {
   columnDefs = [
-    { field: 'Track' },
-    { field: 'No. of Requested Resource' },
-    { field: 'Filled Position' },
-    { field: 'Open Position' },
+    { field: 'Track', resizable: true },
+    { field: 'NoofRequestedResource', resizable: true },
+    { field: 'FilledPosition', resizable: true },
+    { field: 'OpenPosition', resizable: true },
   ];
+
   rowData = [
     {
       Track: '.Net',
       NoofRequestedResource: 11,
       FilledPosition: 4,
       OpenPosition: 7,
+    },
+    {
+      Track: 'BA &SQA',
+      NoofRequestedResource: 24,
+      FilledPosition: 10,
+      OpenPosition: 14,
+    },
+  ];
+
+  columnDefs1 = [
+    { field: 'Track', resizable: true },
+    { field: 'TotalInterviews', resizable: true },
+    { field: 'Pass', resizable: true },
+    { field: 'Failed', resizable: true },
+    { field: 'Deferred', resizable: true },
+    { field: 'PendingFeedback', resizable: true },
+    { field: 'PendingHiringGrid', resizable: true },
+    { field: 'Hired', resizable: true },
+  ];
+
+  rowData1 = [
+    {
+      Track: '.Net',
+      TotalInterviews: 11,
+      Pass: 4,
+      Failed: 7,
+      Deferred: 11,
+      PendingFeedback: 3,
+      PendingHiringGrid: 11,
+      Hired: 7,
+    },
+    {
+      Track: 'BA & SQA',
+      TotalInterviews: 11,
+      Pass: 4,
+      Failed: 7,
+      Deferred: 11,
+      PendingFeedback: 3,
+      PendingHiringGrid: 11,
+      Hired: 7,
     },
   ];
 
